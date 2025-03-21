05a22e0f332f80443cf90cdd5175886b45c0d886
Sergio Varas <sergiovaras786@gmail.com>
2025-03-21 17:20:16 -0300

    ok

--- chamber/script/directory.js
+++ chamber/script/directory.js
@@ -16,7 +16,7 @@ function displayMembers(members, view) {
       card.classList.add('member-card');
       card.innerHTML = `
         <h2>${member.name}</h2>
-        <img src="image/${member.image}" alt="${member.name}">
+        <img src="${member.image}" alt="${member.name}">
         <p>${member.address}</p>
         <p>${member.phone}</p>
         <a href="${member.website}">Sitio web</a>
